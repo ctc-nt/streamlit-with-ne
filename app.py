62611d77d3ab69f233d16090369f293e36e6021e
Tomoya Watanabe <tomoya.watanabe@ctc-g.co.jp>
2023-12-17 00:40:50 +0900

    Formの外側だけ作った

--- app.py
+++ app.py
@@ -2,16 +2,7 @@ from backend import get_system_version,get_users,get_tty
 import streamlit as st
 import pandas as pd
 
-# df = pd.json_normalize(res, record_path=["info"])
-# df
-
-menu = [
-    "top",
-    "users",
-    "tty"
-]
-
-selected_menu = st.sidebar.radio("Menu",menu)
+selected_menu = st.sidebar.radio("Menu",["top","users","tty"])
 
 if selected_menu == "top":
     res = get_system_version()
@@ -23,17 +14,34 @@ if selected_menu == "users":
     res = get_users()
     df = pd.json_normalize(res["users"])
     df
+
+    "Create User"
     "参考 : 取得してきた生のjson"
     res
 
-    "Create User"
 
 if selected_menu == "tty":
     "Current tty Confugiration"
     res = get_tty()
     df = pd.json_normalize(res["ttylist"])
     df
+
+    baud_list=["9600","115200"]
+    bitchar_list=["8"]
+    parity_list=["none"]
+    stop_list=["1"]
+    flow_list=["none"]
+    detect_dsr_list=["on","off"]    
+
+    "Modify tty Configuration"
+    tty = st.selectbox("tty",res["ttylist"],format_func=lambda x : x["tty"],index=0)
+    baud = st.selectbox("baud",baud_list)
+    bitchar = st.selectbox("bitchar",bitchar_list)
+    parity = st.selectbox("parity",parity_list)
+    stop = st.selectbox("stop",stop_list)
+    flow = st.selectbox("flow",flow_list)
+    detect_dsr = st.selectbox("detect_dsr",detect_dsr_list)
+
     "参考 : 取得してきた生のjson"
     res
 
-    "Modify tty"
